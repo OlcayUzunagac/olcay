8299a6f0c8c963a1586b5330e84fd8b173d7c011
raxonen <65630875+raxonen@users.noreply.github.com>
2020-05-20 00:38:52 +0200

    Add files via upload

---
+++ Question4.java
@@ -0,0 +1,22 @@
+package Day_2;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 
+		 * public class Main{
+		 * 
+		 * 		public static void main(String[] args){
+		 * 
+		 * 		System.out.println("Q");
+		 * 
+		 * 		}
+		 * }
+		 * 
+		 */
+
+	}
+
+}
